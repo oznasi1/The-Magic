d6cfc970d6c5c3824d21215fb1ebaa2a5901c9e2
oznasi1 <oznasi1@gmail.com>
2020-01-17 20:11:26 +0200

    Updating gatsby-config

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,25 @@
-import React ,{useState} from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
-import magics from '../data.json'
+import magics from "../data.json"
 import Header from "./header"
 import Content from "./content"
 import "./layout.css"
 import "./content.css"
 
 const Layout = () => {
+  const randomMagicIndex = () => Math.floor(Math.random() * 27 + 0)
 
-  const randomMagicIndex = () => Math.floor((Math.random() * 27) + 0);
+  const [magicIndex, setMagicIndex] = useState(randomMagicIndex)
 
-  const [magicIndex,setMagicIndex] = useState(randomMagicIndex);
-
-  const _setMagicIndex = () => setMagicIndex(randomMagicIndex);
+  const _setMagicIndex = () => setMagicIndex(randomMagicIndex)
 
   return (
-    <div className='container'>
-      <Header siteTitle={magics[magicIndex].title} magicIndex={ magicIndex }/>
+    <div className="container">
+      <Header siteTitle={magics[magicIndex].title} magicIndex={magicIndex} />
       <Content content={magics[magicIndex].content} />
-      <button className='button' onClick={ _setMagicIndex } >Another magic</button>
+      <button className="button" onClick={_setMagicIndex}>
+        קסם אחר
+      </button>
     </div>
   )
 }

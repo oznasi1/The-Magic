8da7cec64ca1db8cef2c2b1b2ccb76ed94d3653c
oznasi1 <oznasi1@gmail.com>
2020-09-16 14:12:01 +0300

    Add bootstrap and update design

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,34 +3,44 @@ import PropTypes from "prop-types"
 import magics from "../data.json"
 import Header from "./header"
 import Content from "./content"
+import { Button } from "react-bootstrap"
 import "./layout.css"
 import "./content.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Layout = () => {
   const randomMagicIndex = () => Math.floor(Math.random() * 27 + 0)
 
-  const [magicIndex, setMagicIndex] = useState(randomMagicIndex)
+  const [magicIndex, setMagicIndex] = useState(0)
 
   const setRandomMagicIndex = () => setMagicIndex(randomMagicIndex)
 
   const increaseMagicIndex = () => setMagicIndex(magicIndex + 1)
 
-  const decreaseMagicIndex = () => setMagicIndex(magicIndex - 1)
+  const decreaseMagicIndex = () =>
+    setMagicIndex(magicIndex > 0 ? magicIndex - 1 : 0)
 
   return (
-    <div className="container">
+    <div className="app-container">
       <Header siteTitle={magics[magicIndex].title} magicIndex={magicIndex} />
       <Content content={magics[magicIndex].content} />
       <div className="buttonsContainer">
-        <button className="button" onClick={decreaseMagicIndex}>
+        <Button
+          type="button"
+          class="btn btn-primary"
+          className="button"
+          onClick={decreaseMagicIndex}
+        >
           קסם קודם
-        </button>
-        <button className="button" onClick={increaseMagicIndex}>
+        </Button>
+        <Button
+          type="button"
+          class="btn btn-primary"
+          className="button"
+          onClick={increaseMagicIndex}
+        >
           קסם הבא
-        </button>
-        <button className="button" onClick={setRandomMagicIndex}>
-          קסם אחר
-        </button>
+        </Button>
       </div>
     </div>
   )

20b13812ee2b0f0b3214e7daf2fa8012d2856fb9
oznasi1 <oznasi1@gmail.com>
2020-10-03 20:08:01 +0300

    Design improvement

--- src/components/content.js
+++ src/components/content.js
@@ -1,9 +1,13 @@
 import React from "react"
 
 const Content = ({ content }) => (
-  <div style={{ overflowY: "auto", height: "355px" }}>
+  <div style={{ overflowY: "auto", height: "355px", margin: "10px" }}>
     {content.map((step, index) => (
-      <div className="step">{`${index + 1}. ${step}`}</div>
+      <div style={{ display: "flex", alignItems: "baseline" }}>
+        <span style={{ color: "#0069D9", fontWeight: 700 }}>{`${index +
+          1}. `}</span>
+        <div className="step">{`${step}`}</div>
+      </div>
     ))}
   </div>
 )
